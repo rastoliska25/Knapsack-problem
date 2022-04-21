9a8864cd5596535e2075f59b354428f6a5d5eded
rliska <rliska@RLISKA>
2022-04-21 07:08:33 +0200

    Add project files.

---
+++ KnapSackGraphic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knapsack_problem
+{
+    public partial class KnapSackGraphic : Form
+    {
+        public KnapSackGraphic()
+        {
+            InitializeComponent();
+        }
+    }
+}
